b4d2476f706a1ecfc12ee1469f85747eb00d6f50
Haritha B <haritha2seven@gmail.com>
2024-06-19 23:38:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (37.30%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length-1;
+        int low=0, mid =0 ;
+        int high = n;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+
+                high--;
+                
+            }
+        }
+        
+    }
+}
