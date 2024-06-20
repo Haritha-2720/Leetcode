626634485ae444e8095e25af2f888c666623a3db
Haritha B <haritha2seven@gmail.com>
2024-06-20 11:51:00 +0530

    Time: 1 ms (99.83%) | Memory: 53.4 MB (9.63%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int ele=nums[0];
+        int count=1;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(ele==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+             if(count==0){
+                
+                ele=nums[i];
+                count=1;
+            }
+            
+        }
+        return ele;
+    }
+}
