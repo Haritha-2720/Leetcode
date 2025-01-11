776b3f32d697d9a245755f9ca99636dc89e121a6
Haritha B <haritha2seven@gmail.com>
2025-01-11 22:20:02 +0530

    Time: 5 ms (72.00%) | Memory: 43.9 MB (70.00%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-     if(x<0 || x!=0 && x%10 ==0 ) return false;
-    int saveNum=0;
-    while(x>saveNum){
-        saveNum = saveNum*10 + x%10;
-        x/=10;
+
+         if(x<0){
+            return false;
+        }
+
+        int store =0;
+
+        int temp=x;
+
+        while(x!=0){
+            int c = x%10;
+            store =(store*10)+c;
+            x=x/10;
+
+        }
+        return store==temp;
+        
     }
-    return (x==saveNum || x==saveNum/10);
-}
 }
