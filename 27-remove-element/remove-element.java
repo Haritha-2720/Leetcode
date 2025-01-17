5b4e8b657aee442ffab4362e6bebcf291363b08c
Haritha B <haritha2seven@gmail.com>
2025-01-17 22:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (43.34%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -5,12 +5,15 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
+
                 nums[count]=nums[i];
-                count++;
+                 count++;
+
             }
         }
         return count;
+
+
         
     }
-    
 }
